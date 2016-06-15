018b162501cab4ff97b1a2d41d51053d9268c607
WillemElfring <willemelfring@hotmail.com>
2016-06-15 20:56:28 +0200

    versie1

---
+++ haalfotos.js
@@ -0,0 +1,41 @@
+/**
+ * Created by Willem on 13-6-2016.
+ */
+$(document).ready(function() {
+    var Search;
+    $('#button').click(function () {
+        Search = $('#zoeken').val();
+        haalFoto();
+    });
+    $('#zoeken').keydown(function(e){
+        if(e.keyCode == 13){
+            Search = $(this).val();
+            haalFoto();
+        }
+    });
+    
+    function haalFoto(){
+       var flickrURL = "http://api.flickr.com/services/feeds/photos_public.gne?format=json&tags=" + Search +"&jsoncallback=?"
+       
+       $.ajax (
+       {
+           dataType: 'json',
+           method: 'GET',
+           url: flickrURL,
+           success: verwerkFotos
+       }
+       )
+    }
+    
+    function verwerkFotos(data){
+        console.log(data);
+        $('#fotos').html("");
+        for(var i=0; 1<data.items.length; i++){
+            var foto = data.items[i];
+            var htmlCode = "<div id='container'><div class='houder'><div class='afbeelding>'<a href='" + foto.link + "' target='_blank'><img src='" + foto.media.m + "' alt='" + foto.title + "' ></a></div><h4>" + foto.title + "</h4></div></div>";
+            $('#fotos').append(htmlCode);
+        }
+    }
+    
+    
+})
